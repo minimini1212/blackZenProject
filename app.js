559b69b8f43e9a638cb13d38c52ae2eb5af3ed4f
minimini1212 <zjonyv12@naver.com>
2025-06-02 00:15:27 +0900

    feat: dns/promises를 통한 ip 확인

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const app = express();
-app.use(express.static("Asset"));
 
-app.get("/api/chart", async (req, res) => {
-  try {
-    const html = await axios.get("https://www.genie.co.kr/chart/top200");
-    const $ = cheerio.load(html.data);
-    const bodyList = $("tr.list");
-    const ulList = [];
+// const dns = require('dns');
+// const dnsPromises = dns.promises;
 
-    bodyList.each((i, element) => {
-      ulList.push({
-        rank: i + 1,
-        title: $(element).find("td.info a.title").text().replace(/\s/g, ""),
-        artist: $(element).find("td.info a.artist").text().replace(/\s/g, ""),
-      });
-    });
+// const options = {
 
-    console.log(ulList);
+//     family: 6,
+//     hints: dns.ADDRCONFIG | dns.V4MAPPED,
+// };
 
-    res.json(ulList);
-  } catch (err) {
-    res.status(500).json({ error: "크롤링 실패" });
-  }
-});
+// const ip = await dnsPromises.lookup('geeksforgeeks.org', options);
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000 실행 중");
-});
+const dns = require('dns/promises');
+
+async function main() {
+    const ip = await dns.lookup('tistory.com');
+    console.log(ip);
+}
+
+main();
+
+// app.use(express.static("Asset"));
+
+// app.get("/api/chart", async (req, res) => {
+//   try {
+//     const html = await axios.get("https://www.genie.co.kr/chart/top200");
+//     const $ = cheerio.load(html.data);
+//     const bodyList = $("tr.list");
+//     const ulList = [];
+
+//     bodyList.each((i, element) => {
+//       ulList.push({
+//         rank: i + 1,
+//         title: $(element).find("td.info a.title").text().replace(/\s/g, ""),
+//         artist: $(element).find("td.info a.artist").text().replace(/\s/g, ""),
+//       });
+//     });
+
+//     console.log(ulList);
+
+//     res.json(ulList);
+//   } catch (err) {
+//     res.status(500).json({ error: "크롤링 실패" });
+//   }
+// });
+
+// app.listen(3000, () => {
+//   console.log("http://localhost:3000 실행 중");
+// });
